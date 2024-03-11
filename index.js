693622e1e16e5d72ee294a2f4c170bdabc5980f3
BobHawkins619 <110990493+BobHawkins619@users.noreply.github.com>
2024-03-11 20:19:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-const myArray = [1, 2, 3];
-
 // Define a function that returns the index of a given value in the array
-function findIndex(value) {
-  return myArray.indexOf(value);
+function findIndex(array, index) {
+  return array.indexOf(index);
 }
 
 // Export the function to make it accessible to other modules
